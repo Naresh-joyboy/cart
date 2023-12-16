4aa457d450a02002a1c94f2c05a71a729947b36c
arun <arun@relightechnologies.com>
2023-12-16 22:11:29 +0530

    code changed with three column

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,32 +5,33 @@ import './total.css'
 
 const Cart = ({ item, handleclick }) => {
   return (
-    <div className="cartt">
-      <div className="col-md-4 mb-3">
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src={item.img} />
-          <Card.Body>
-            <Card.Title>{item.name}</Card.Title>
-            <Card.Text>{item.price}</Card.Text>
-            <Button className='btnn'variant="primary" onClick={() => {handleclick(item)}}>Add cart</Button>
-          </Card.Body>
-        </Card>
-      </div>
+    <div className="col-md-4 mb-3">
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={item.img} />
+        <Card.Body>
+          <Card.Title>{item.name}</Card.Title>
+          <Card.Text>{item.price}</Card.Text>
+          <Button className='btnn' variant="primary" onClick={() => { handleclick(item) }}>Add cart</Button>
+        </Card.Body>
+      </Card>
     </div>
   )
 }
 
-const Home = ({ handleclick ,productItems}) => {
-  
+const Home = ({ handleclick, productItems }) => {
+  //  Array.from({ length: Math.ceil(productItems.length / 3) } -----> create a create /object with how many threes present in whole cart,
+  // (_, index) => productItems.slice(index * 3, index * 3 + 3)); -----> "_"  unused or empty parameted and index of productitems 
+  const chunkedProductItems = Array.from({ length: Math.ceil(productItems.length / 3) }, (_, index) => productItems.slice(index * 3, index * 3 + 3));
+
   return (
     <div className="container">
-      <div className="row">
-        <div className="col-md-3">
-          {productItems.map((item) => (
+      {chunkedProductItems.map((row, rowIndex) => (
+        <div key={rowIndex} className="row">
+          {row.map((item) => (
             <Cart key={item.id} item={item} handleclick={handleclick} />
           ))}
         </div>
-      </div>
+      ))}
     </div>
   );
 };
